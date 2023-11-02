99b5ccfd43708746a64df8690d7be6460853d6bb
Vreyzov <115491934+Vreyzov@users.noreply.github.com>
2023-11-02 22:36:37 +0300

    Add files via upload

---
+++ 03.11.23.cs
@@ -0,0 +1,42 @@
+﻿// стр 13, № 2
+
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (x*x + y * y >= 9 && x*x + y*y <= 25 && x >= 0)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("Не находится");
+//}
+
+// стр 13, № 3
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y<=4 && y >= 0&& x >= -2 && x <=4 || y >= -3 && y <= 0 && x >= -4 && x <= 5)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("Не находится");
+//}
+
+// стр 13, № 4
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y >= 5 - x && y >= x -3 && y <= 3 + x && y <= -x + 11)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("Не находится");
+//}
